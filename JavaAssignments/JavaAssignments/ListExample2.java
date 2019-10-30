d25e170e6e4c04338a4a2c9cd2dd30fae11c0c96
Automation <nidit@192.168.0.12>
2019-10-29 22:10:06 -0400

    This is new file

---
+++ JavaAssignments/JavaAssignments/ListExample2.java
@@ -0,0 +1,24 @@
+package JavaAssignments;
+
+import java.util.ArrayList;
+
+public class ListExample2 {
+
+	public static void main(String[] args)
+	{
+		ArrayList aList = new ArrayList();
+		
+		aList.add("Test");
+		aList.add(123);
+		aList.add(null);
+		aList.add(55.22);
+		System.out.println("Size of the list" + aList.size());
+		
+		
+		
+			
+			
+
+	}
+
+}
